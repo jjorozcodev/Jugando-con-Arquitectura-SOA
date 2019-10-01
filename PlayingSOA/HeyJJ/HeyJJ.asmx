2a35211dd981fea5804fd30978bbf1d3020c6aab
JJ Orozco López <jjorozcodev@gmail.com>
2019-09-30 23:30:52 -0600

    Servicio Web para saludar
    Se añade solución en blanco.
    Posterior, se añade proyecto ASP .Net con estructura de Webforms.
    Se modifica método "Hello world" por "Greeting" para dar saludo personalizado al invocar web method.

---
+++ PlayingSOA/HeyJJ/HeyJJ.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="HeyJJ.asmx.cs" Class="HeyJJ.HeyJJ" %>
---
+++ PlayingSOA/HeyJJ/HeyJJ.asmx.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Services;
+
+namespace HeyJJ
+{
+    /// <summary>
+    /// Summary description for HeyJJ
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class HeyJJ : WebService
+    {
+
+        [WebMethod]
+        public string Greeting()
+        {
+            return "Hey, I'm JJ! How's it going?";
+        }
+    }
+}
