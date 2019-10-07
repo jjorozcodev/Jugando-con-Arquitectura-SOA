41646857b32632a48b47023ea16115db984ee403
JJ Orozco López <jjorozcodev@gmail.com>
2019-10-06 20:13:13 -0600

    Servicio WCF a partir de ASMX
    Se completa adaptación de servicio ASMX como Servicio WCF, usando interfaces y servicio como tal. Se redirige archivo svc apuntando al servicio que hereda de webservice y la interfaz de contrato para WCF.

---
+++ PlayingSOA-WCF/wsRegistroDepositos/IServicioDepositos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace wsRegistroDepositos
+{
+    [WebService(Namespace = "https://www.uca.edu.ni/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ScriptService]
+    [ServiceContract(Namespace = "https://www.uca.edu.ni")]
+    public interface IServicioDepositos
+    {
+        [WebMethod]
+        [OperationContract]
+        int AgregarDeposito(int cantidad, int idUsuario);
+
+        [WebMethod]
+        [OperationContract]
+        int AgregarUsuario(string nombre, int meta);
+
+        [WebMethod]
+        [OperationContract]
+        List<Usuario> ListaUsuarios();
+    }
+}
---
+++ PlayingSOA-WCF/wsRegistroDepositos/wsRegistroDepositos.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Services;
+
+namespace wsRegistroDepositos
+{
+    [WebService(Namespace = "https://www.uca.edu.ni/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class wsRegistroDepositos : WebService, IServicioDepositos
+    {
+        private UsuarioRepositorio repositorio = new UsuarioRepositorio();
+
+        public int AgregarDeposito(int cantidad, int idUsuario)
+        {
+            Usuario u = repositorio.ObtenerPorId(idUsuario);
+            if (u == null)
+            {
+                return -1;
+            }
+            u.Total += cantidad;
+            repositorio.Guardar(u);
+            return u.Total;
+        }
+
+        public int AgregarUsuario(string nombre, int meta)
+        {
+            Usuario u = new Usuario { Nombre = nombre, Meta = meta, Total = 0 };
+            repositorio.Agregar(u);
+            return u.UsuarioId;
+        }
+
+        public List<Usuario> ListaUsuarios()
+        {
+            return new List<Usuario>(repositorio.ObtenerTodos());
+        }
+    }
+}
